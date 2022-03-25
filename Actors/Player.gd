88b9195402ed9df1b7059f94925c0e67297604c6
Shoter99 <shoter998@gmail.com>
2022-03-25 18:25:39 +0100

    added script to move left and right

--- Actors/Player.gd
+++ Actors/Player.gd
@@ -1,3 +1,7 @@
 extends Actor
 
-
+func _physics_process(delta):
+	var dir: = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 1.0)
+	
+	velocity = speed * dir
+	
