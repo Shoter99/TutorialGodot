3ca9f2b727193e09420d1e6b79e7e89905e42762
Shoter99 <shoter998@gmail.com>
2022-03-25 20:24:11 +0100

    added enemy and stomping on enemy

---
+++ Actors/Enemy.gd
@@ -0,0 +1,20 @@
+extends "res://Actors/Actor.gd"
+
+func _ready():
+	set_physics_process(false)
+	velocity.x = -speed.x
+
+
+func _on_StompDetector_body_entered(body):
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionPolygon2D").disabled = true
+	queue_free()
+
+
+func _physics_process(delta) -> void:
+	velocity.y += gravity*delta
+	if is_on_wall():
+		velocity.x *= -1.0
+	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
+
